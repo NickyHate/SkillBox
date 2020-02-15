c6bbc76107afdd1e2a685540ac04881ac4be5790
Никита <nikitostokarev22@gmail.com>
2020-02-16 00:05:11 +0300

    Fix error with dublicates in 9 module

--- Module9/FunctionObrabotchik/script.js
+++ Module9/FunctionObrabotchik/script.js
@@ -4,13 +4,28 @@ const ol = document.querySelector("ol");
 const li = document.querySelector("li");
 
 btn.addEventListener("click", function() {
+  debugger;
   let a = work.value;
+  let arr = document.querySelectorAll("li");
   if (a == "") {
     alert("Не заполнено поле!");
   } else {
     let b = document.createElement("li");
     b.innerHTML = a;
     ol.append(b);
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].textContent == a) {
+      let agree = confirm(
+        "Вы уверены, что хотите добавить дублирующую задачу?"
+      );
+      if (agree === true) {
+        break;
+      } else {
+        ol.removeChild(ol.lastChild);
+        break;
+      }
+    }
   }
 });
 
