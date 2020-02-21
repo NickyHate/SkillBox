af53b522e4381d54f6bb4e866c7bf73e162cd834
Никита <nikitostokarev22@gmail.com>
2020-02-21 23:25:35 +0300

    Rewrite module11 translator with fetch

--- Module11/Translator.UseYandexAPI/script.js
+++ Module11/Translator.UseYandexAPI/script.js
@@ -1,5 +1,4 @@
-var API_KEY =
-  "trnsl.1.1.20200215T225608Z.add5acd109a5b4f2.210ab5185caf6fa4075d38061fc58ca1cbe3c525";
+/* Всплытие меню с выбором языка */
 const arrow = document.querySelectorAll(".arrow");
 for (let i = 0; i < arrow.length; i++) {
   arrow[i].addEventListener("click", function() {
@@ -7,7 +6,7 @@ for (let i = 0; i < arrow.length; i++) {
     this.parentElement.nextElementSibling.classList.toggle("active");
   });
 }
-
+/* Переключение языков */
 const lang = document.querySelectorAll(".language");
 
 for (let i = 0; i < lang.length; i++) {
@@ -25,15 +24,11 @@ const button = document.querySelector(".button");
 
 button.addEventListener("click", getTranslate);
 
+var API_KEY =
+  "trnsl.1.1.20200215T225608Z.add5acd109a5b4f2.210ab5185caf6fa4075d38061fc58ca1cbe3c525";
+var url = "https://translate.yandex.net/api/v1.5/tr.json/translate";
 function getTranslate() {
-  /* Создали новый запрос */
-  var req = new XMLHttpRequest();
-
-  /* Добавили url запроса */
-
-  var url = "https://translate.yandex.net/api/v1.5/tr.json/translate";
-
-  /* Добавили переменную для передачи текста запроса */
+  var translate = document.querySelector(".translate");
 
   var getWord = document.querySelector(".get__word");
   var word = "&text=" + getWord.value;
@@ -61,23 +56,11 @@ function getTranslate() {
   url += word;
   url += "&lang=" + firstLang + "-" + secondLang;
 
-  var translate = document.querySelector(".translate");
-
-  req.addEventListener("load", function() {
-    console.log(req.response);
-    var response = JSON.parse(req.response);
-    if (response.code !== 200) {
-      translate.innerHTML =
-        "Произошла ошибка при получении ответа от сервера:\n\n" +
-        response.message;
-      return;
-    }
-    if (response.text.length === 0) {
-      translate.innerHTML = "К сожалению, перевод для данного слова не найден";
-    }
-    translate.innerHTML = response.text.join("<br>");
-  });
-
-  req.open("get", url);
-  req.send();
+  async function getResponse() {
+    var response = await fetch(url);
+    var content = await response.json();
+    content = content.text;
+    translate.innerHTML = content[content.length - 1];
+  }
+  getResponse();
 }
